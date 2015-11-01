04d0927a1e574a309d24b0aa143853fcbd192dfb
Ralf Richter <entwickler42@gmail.com>
2015-11-01 19:37:32 +0100

    continued german translation

--- QtCacheTool/qtcachetool_de_DE.ts
+++ QtCacheTool/qtcachetool_de_DE.ts
@@ -6,122 +6,122 @@
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <source>Qt Caché Tool</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="25"/>
         <source>Caché Connection String</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="40"/>
         <source>Target UCI</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziel UCI</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="50"/>
         <source>&amp;Select</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Auswählen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="66"/>
         <source>Import Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateien einlesen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="93"/>
         <source>&amp;Add</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Hinzufügen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="100"/>
         <source>&amp;Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Entfernen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="120"/>
         <source>&amp;Ignore Errors</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Fehler ignorieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="130"/>
         <source>&amp;Compile</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Kompilieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="146"/>
         <source>ckf</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="156"/>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>I&amp;mportieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="166"/>
         <source>Export Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateien exportieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="174"/>
         <source>Output Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausgabeverzeichnis</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="189"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="200"/>
         <source>Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Filter</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="208"/>
         <source>Include</source>
-        <translation type="unfinished"></translation>
+        <translation>Einschließen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="249"/>
         <source>Exclude</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausschließen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="304"/>
         <source>&amp;Export</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Exportieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="320"/>
         <source>&amp;Abort</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Abbrechen</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="349"/>
         <source>&amp;Program</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Anwendung</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="356"/>
         <source>?</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="366"/>
         <source>&amp;Import Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateien &amp;importieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="371"/>
         <source>&amp;Export Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateien &amp;exportieren</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="376"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Informationen</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="85"/>
@@ -132,33 +132,33 @@
         <location filename="mainwindow.cpp" line="177"/>
         <location filename="mainwindow.cpp" line="196"/>
         <source>Exception</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausnahmefehler</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="87"/>
         <location filename="mainwindow.cpp" line="116"/>
         <source>Unknown exception occured!</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannter Ausnahmefehler ist aufgetreten!</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="92"/>
         <source>Sucessfully connected to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Verbindung zu %1 erfolgreich hergestellt</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="93"/>
         <source>Failed to connected to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim verbinden zu %1</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="50"/>
         <source>Welcome to the Qt Caché Tool</source>
-        <translation type="unfinished"></translation>
+        <translation>Willkommen zum Qt Caché Tool</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="56"/>
         <source>Selected target UCI: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziel UCI ausgewählt: %1</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="123"/>
@@ -166,44 +166,44 @@
         <location filename="mainwindow.cpp" line="138"/>
         <location filename="mainwindow.cpp" line="190"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="123"/>
         <source>No items selected!</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Einträge ausgewählt!</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="134"/>
         <location filename="mainwindow.cpp" line="190"/>
         <source>Cachè connection has not been established yet!</source>
-        <translation type="unfinished"></translation>
+        <translation>Verbindung zu Caché wurde noch nicht hergestellt!</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="138"/>
         <source>List of files is empty!</source>
-        <translation type="unfinished"></translation>
+        <translation>Liste der Dateien ist leer!</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="152"/>
         <source>Importing %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Importiere %1</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="158"/>
         <source>%1
 %2</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="171"/>
         <source>Import aborted!</source>
-        <translation type="unfinished"></translation>
+        <translation>Import abgebrochen!</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="174"/>
         <source>Import finished!</source>
-        <translation type="unfinished"></translation>
+        <translation>Import abgeschlossen!</translation>
     </message>
 </context>
 </TS>
