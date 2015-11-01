2b12dc05c80f532a930a757e0b5e23f1a6953f13
Ralf Richter <entwickler42@gmail.com>
2015-11-01 15:58:10 +0100

    added Cachè include files to default name filters

--- QtCacheUi/qtcacheui.cpp
+++ QtCacheUi/qtcacheui.cpp
@@ -10,8 +10,8 @@ QtCacheUi::QtCacheUi()
 QStringList QtCacheUi::defaultNameFilters()
 {
     return QStringList()
-            << QObject::tr("Caché (*.xml *.mac *.int *.cls *.gbl)", "QtCacheUi")
-            << QObject::tr("Caché Sources (*.mac *.int *.cls )", "QtCacheUi")
+            << QObject::tr("Caché (*.xml *.mac *.int *.cls *.inc *.gbl)", "QtCacheUi")
+            << QObject::tr("Caché Sources (*.mac *.int *.cls *.inc)", "QtCacheUi")
             << QObject::tr("Caché Globals (*.gbl)", "QtCacheUi")
             << QObject::tr("Any Files (*)", "QtCacheUi");
 }
